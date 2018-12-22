ddb627c9914c56bda638659c7efac6bcfc1cd7db
lucaswiix <lucasrocha.rosaborges@hotmail.com>
2018-12-22 19:53:42 -0300

    Exemple Index

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Wiix TwitchTV Notifier</title>
+	 
+    <!-- fontawesome for arrow -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/solid.css" integrity="sha384-+0VIRx+yz1WBcCTXBkVQYIBVNEFH1eP6Zknm16roZCyeNg2maWEpk/l/KsyFKs7G" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/fontawesome.css" integrity="sha384-jLuaxTTBR42U2qJ/pm4JRouHkEDHkVqH0T1nyQXn1mZ7Snycpf6Rl25VBNthU4z0" crossorigin="anonymous">
+
+	<!-- Twitch.css  -->
+	<link rel="stylesheet" type="text/css" href="twitch.css">
+
+</head>
+<body>
+
+
+
+
+
+<!-- wtw.js : Main JS File to notifier -->
+<script src="wtw.js"></script>
+<!-- JQuery(we dont ill use it)-->
+<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>	
+		
+</body>
+</html>
